a2097dbb2188b22f5545a709355328f2995895a7
tinchopapa56 <martinlabastie.pp@gmail.com>
2022-12-27 17:22:05 -0300

    skills updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,17 +5,23 @@ import Guitar from "../../img/guitar.png"
 import Bicep from "../../img/feel the burn.png"
 import Meditation from "../../img/lotus pose.png"
 import Thinker from "../../img/equation.png"
-import { FaHtml5,FaCss3Alt,FaReact,FaJsSquare,FaGitAlt,FaGit } from "react-icons/fa";
-import { SiRedux,SiMongodb,SiBootstrap,SiFirebase } from "react-icons/si";
-import { DiNodejs, DiNpm } from "react-icons/di";
 
+import I0 from "../../img/skills/1.png"
+import I1 from "../../img/skills/2 (1).png"
+import I2 from "../../img/skills/2 (1).jpg"
+import I3 from "../../img/skills/2 (1).webp"
+import I4 from "../../img/skills/2 (2).png"
+import I5 from "../../img/skills/2 (2).webp"
+import I6 from "../../img/skills/2 (3).png"
+import I7 from "../../img/skills/2 (4).png"
+import I8 from "../../img/skills/2 (5).png"
 
 export default function About(){
     return(
     <article className="a">
          
         <section className="about-columns" id="column-left"> 
-            <img className="absolute-column-img" src={Guitar} alt="Guitar" />
+        <img className="absolute-column-img" src={Meditation} alt="Meditation" />
             <img className="absolute-column-img" src={Gym} alt="Gym" />
         </section>
 
@@ -28,28 +34,51 @@ export default function About(){
           <p className="a-desc">Hobbies: Guitar, Gym, TaeKwonDo.</p>
           <h3 id="about-h3">Skills:</h3>
             </div>
-            <div className="skills-grid"> 
-                    <FaHtml5 className="skills-grid-item" />
-                    <FaCss3Alt className="skills-grid-item" />
-                    <FaGit className="skills-grid-item" />
-                    <FaGitAlt className="skills-grid-item" />
-                    <FaJsSquare className="skills-grid-item" />
-                    <FaReact className="skills-grid-item" />
-                    <SiBootstrap className="skills-grid-item" />
-                    <SiFirebase className="skills-grid-item" />
-                    <SiMongodb className="skills-grid-item" />
-                    <SiRedux className="skills-grid-item" />
-                    <DiNodejs className="skills-grid-item" />
-                    <DiNpm className="skills-grid-item" />
+
+            <div className="rotator">
+                <div className="items">
+                    <img src={I0} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I1} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I2} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I3} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I4} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I5} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I7} alt="Image Alt" />
+                </div>
+                <div className="items">
+                    <img src={I6} alt="Image Alt" />
+                </div>
+                
+                <div className="items">
+                    <img src={I8} alt="Image Alt" />
+                </div>
             </div>
+
             <div className="last_image"> 
                 <img className="thinker" src={Thinker} alt="front end engineer" />
             </div>
         </section>
 
+        {/*  3d*/}
+        
+        {/* ends 3d */}
+
             <section className="about-columns">
                 <img className="absolute-column-img" src={Bicep} alt="Bicep" />
-                <img className="absolute-column-img" src={Meditation} alt="Meditation" />
+                <img className="absolute-column-img" src={Guitar} alt="Guitar" />
+                
             </section>
 
     </article>
