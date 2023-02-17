90ae41d2749d09930358158be255b16c115bc602
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2023-02-17 17:06:45 -0300

    added meetlify

--- src/components/portfolio/projectsArray.js
+++ src/components/portfolio/projectsArray.js
@@ -1,7 +1,5 @@
 import alk from "../svgs/a.svg"
-import b2 from "../svgs/b2.svg"
 import w2 from "../svgs/w2.svg"
-import m from "../svgs/m.svg"
 import L from "../../img/Landing.png"
 import ML from "../../img/ml.png"
 
@@ -20,6 +18,17 @@ export const projectsArray = [
   },
   {
     id: 2,
+    img: alk,
+    t: "Meetlify (C#)",
+    d: "SCRUM team of 4 people using git workflow.",
+    skills: "C#, ASP.NET web API, Entity Framework, SIGNAL R (Livechat/Comments) JWT, Cloudinary, React & Typescript.js",
+    features: "User Auth & Auth, CRUD managment of events, + USERS user can follow each other, comment events & manage their photos.",
+    url: "https://go-clothing-twxt.vercel.app/",
+    GIT: "https://github.com/Hernanbp/GoClothing",
+    tag: "🏆 CRUD: FULLSTACK(C#-Typescript)"
+  },
+  {
+    id: 3,
     img: ML,
     t: "ML Challenge",
     d: "Front-end (take home project) built with Chakra UI & Typescript",
@@ -29,7 +38,7 @@ export const projectsArray = [
     bg: "yellow"
   },
   {
-    id: 3,
+    id: 4,
     img: L,
     t: "Start up Landing page",
     d: "RESPONSIVE build with TYPESCRIPT and CHAKRA UI in aproximately 4 hours",
@@ -40,7 +49,7 @@ export const projectsArray = [
     bg: "#f77692",
   },
   {
-    id: 4,
+    id: 5,
     img: w2,
     t: "Workers List",
     d: "Completely functional CRUD application in wchich you can manage the state of your employees",
@@ -49,15 +58,15 @@ export const projectsArray = [
     GIT: "https://github.com/tinchopapa56/WorkersFrontHeroku-Deployed_MERN-",
     tag: "🧱 MERN app: FULLSTACK"
   },
-  {
-    id: 5,
-    img: m,
-    t: "Musicianship Training",
-    d: "React app for ear training. Original proyect idea, original code written from scratch.",
-    url: "https://tinchopapa56.github.io/MusicT/",
-    GIT: "https://github.com/tinchopapa56/MusicT",
-    tag: "🏠 Landing Page: FRONTEND",
-    bg: "background-image: linear-gradient(to right top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);"
-  },
+  // {
+  //   id: 6,
+  //   img: m,
+  //   t: "Musicianship Training",
+  //   d: "React app for ear training. Original proyect idea, original code written from scratch.",
+  //   url: "https://tinchopapa56.github.io/MusicT/",
+  //   GIT: "https://github.com/tinchopapa56/MusicT",
+  //   tag: "🏠 Landing Page: FRONTEND",
+  //   bg: "background-image: linear-gradient(to right top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);"
+  // },
   
 ];
